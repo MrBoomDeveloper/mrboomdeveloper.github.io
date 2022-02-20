5e6bed46c9c68da9c3ca175d9fc3d69c944d65a5
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-02-20 20:56:36 +0500

    updated design, refactored code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
+var header = document.getElementsByTagName("header")[0];
 var burger = document.getElementById("burger");
 var nav = document.getElementsByTagName("nav")[0];
+var more = document.getElementById("load-more");
+var projects = document.getElementById("hidden-projects");
+var activeMenu = false;
+
+window.addEventListener('scroll', function() {
+	if(window.scrollY > 50) {
+		header.classList.add("header-scrolled");
+	} else {
+		if(!activeMenu) {
+			header.classList.remove("header-scrolled");
+		}
+	}
+});
 
 function toggleMenu() {
+	if(activeMenu) {
+		if(window.scrollY < 50) {
+			header.classList.remove("header-scrolled");
+		}
+		activeMenu = false;
+	} else {
+		header.classList.add("header-scrolled");
+		activeMenu = true;
+	}
 	burger.classList.toggle("change");
 	nav.classList.toggle("nav-active");
 }
@@ -12,8 +35,12 @@ function menuSelected() {
 	}
 }
 
-//if(window.location.hash) {
-	//if(window.location.hash.substring(1) == "test") {
-		//document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"experimental.css\" />");
-	//}
-//}
+function menuClose() {
+	menuSelected();
+	return true;
+}
+
+function loadMore() {
+	more.classList.toggle("more-shown");
+	projects.classList.toggle("projects-shown");
+}
