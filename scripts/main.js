4891dffdd80539b87e869c9c0b60551cbc17465f
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-10 23:09:09 +0500

    лала
    втта

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,36 @@
 const header_list = [
-'<li><a href="./#about" onclick="menuClose();">Обо мне</a></li>',
-'<li><a href="./#projects" onclick="menuClose();">Мои проекты</a></li>',
-'<li><a href="mrboomdeveloper+hlm7cjfwxpmqxajeqhsg@boards.trello.com" onclick="menuClose();">Написать мне</a></li>'
+	'<li><a href="./#about" onclick="menuClose();">Обо мне</a></li>',
+	'<li><a href="./#projects" onclick="menuClose();">Мои проекты</a></li>',
+	'<li><a href="mrboomdeveloper+hlm7cjfwxpmqxajeqhsg@boards.trello.com" onclick="menuClose();">Написать мне</a></li>'
 ];
 const footer_list = [[
-'<h4>Ваши данные</h4>',
-'<li><a href="./terms#agreement">Пользовательское соглашение</a></li>',
-'<li><a href="./terms#privacy">Политика конфиденциальности</a></li>',
-'<li><a href="mailto:mrboomdeveloper+hlm7cjfwxpmqxajeqhsg@boards.trello.com">Связаться со мной</a></li>'],
-[
-'<h4>Интересные ссылки</h4>',
-'<li><a href="./resources">Ресурсы и брендинг</a></li>',
-'<li><a href="./archive">Архив данных</a></li>',
-'<li><a target="_blank" href="https://music.youtube.com/playlist?list=PLSWY81108wBhKajj1EUuBr89ydg-DUjPy">Мой плейлист</a></li>']
+	'<h4>Интересные ссылки</h4>',
+	'<li><a href="./resources">Ресурсы и брендинг</a></li>',
+	'<li><a href="./archive">Архив данных</a></li>',
+	'<li><a href="https://bit.ly/mrboomdevplay" target="_blank">Мой плейлист</a></li>'
+], [
+	'<h4>Ваши данные</h4>',
+	'<li><a href="./terms#agreement">Пользовательское соглашение</a></li>',
+	'<li><a href="./terms#privacy">Политика конфиденциальности</a></li>',
+	'<li><a href="mailto:mrboomdeveloper+hlm7cjfwxpmqxajeqhsg@boards.trello.com">Связаться со мной</a></li>'
+]];
+const social_media = [
+	'<li><a href="https://bit.ly/mrboomdevvk" target="_blank"><img src="images/vk.png"/></a></li>',
+	'<li><a href="https://bit.ly/mrboomdevyt" target="_blank"><img src="images/youtube.png"/></a></li>',
+	'<li><a href="https://bit.ly/mrboomdevds" target="_blank"><img src="images/discord.png"/></a></li>'
 ];
 
 let header;
 let burger;
 let nav;
 
-function loaded() {
+function loaded(page) {
 	header = document.getElementsByTagName("header")[0];
 	burger = document.getElementById("burger");
 	nav = document.getElementsByTagName("nav")[0];
 	reveal();
 	addLinks();
+	load(page);
 }
 
 function addLinks() {
@@ -32,16 +38,24 @@ function addLinks() {
 	let footer_links = document.getElementById("footer-links");
 	var header_html = "";
 	var footer_html = "";
+	
 	for(var i = 0; i< header_list.length; i++) {
 		header_html += header_list[i];
 	}
+	
+	footer_html += '<ul id="follow"><h4>Подписывайтесь на меня</h4>';
+	for(var i = 0; i < social_media.length; i++) {
+		footer_html += social_media[i];
+	}
+	footer_html += '</ul>';
 	for(var i = 0; i < footer_list.length; i++) {
-		footer_html += '<ul class="second">';
+		footer_html += '<ul>';
 		for(var i1 = 0; i1 < footer_list[i].length; i1++) {
 			footer_html += footer_list[i][i1];
 		}
 		footer_html += '</ul>';
 	}
+	
 	header_links.innerHTML = header_html;
 	footer_links.innerHTML = footer_html;
 }
@@ -51,11 +65,8 @@ function reveal() {
 	for(var i = 0; i < reveals.length; i++){
 		var windowheight = window.innerHeight;
 		var revealtop = reveals[i].getBoundingClientRect().top;
-		if(revealtop < windowheight){
+		if(revealtop < windowheight)
 			reveals[i].classList.add('active');
-		} else {
-			reveals[i].classList.remove('active');
-		}
 	}
 }
 
--- scripts/project.js
+++ scripts/project.js
@@ -1,43 +1,113 @@
-const names = ["Droid Studio", "FNaF Multiplayer", "Bear's Kitchen"];
-const banners = ["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"];
-const downloads = ["404", "404", "404"];
-const screenshots = [
-	["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"],
-	["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"],
-	["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"]
-];
-const tags = [
-	["Java", "Android", "В разработке", "2022"],
-	["Java", "Android", "Отложено", "2022"],
-	["Scratch", "Android", "Завершено", "2020"]
-];
-const descriptions = [
-	"Мобильная версия Android Studio, но без функции сборки приложений.", 
-	"Мобильный экшен-платформер про выживание и схватку одержимымых аниматроников.", 
-	"Желаете ли вы устроиться в нашей пиццерии? У нас бесплатная пицца для сотрудников!"
-];
+const projects = {
+	"pinned": [{
+		"banner": "images/droidstudio.jpg",
+		"name": "Droid Studio",
+		"tags": ["Java", "Android", "В разработке", "2022"],
+		"screenshots": ["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"],
+		"description": "Мобильная версия Android Studio, но без функции сборки приложений.",
+		"link": "https://nightly.link/MrBoomDeveloper/DroidStudio/workflows/android/master/app-debug.apk.zip"
+	}, {
+		"banner": "images/not_found.jpg",
+		"name": "FNaF Multiplayer",
+		"tags": ["Java", "Android", "Отложено", "2022"],
+		"screenshots": ["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"],
+		"description": "Мобильный экшен-платформер про выживание и схватку одержимымых аниматроников.",
+		"link": "404"
+	}, {
+		"banner": "images/not_found.jpg",
+		"name": "Bear's Kitchen",
+		"tags": ["Scratch", "Android", "Завершено", "2020"],
+		"screenshots": ["images/not_found.jpg", "images/not_found.jpg", "images/not_found.jpg"],
+		"description": "Желаете ли вы устроиться в нашей пиццерии? У нас бесплатная пицца для сотрудников!",
+		"link": "404"
+	}],
+	
+	
+	"archive": [{
+		"name": "Старые игры",
+		"content": [{
+			"banner": "images/not_found.jpg",
+			"name": "Granny 2D",
+			"tags": ["Pocket Code"],
+			"link": "https://share.catrob.at/app/project/77618"
+		}, {
+			"banner": "images/not_found.jpg",
+			"name": "Five Nights at Freddy's 7",
+			"tags": ["Pocket Code"],
+			"link": "https://share.catrob.at/app/project/57826"
+		}, {
+			"banner": "images/not_found.jpg",
+			"name": "FNaF Sniffing Simulator",
+			"tags": ["Java"],
+			"link": "https://gamejolt.com/games/sniffsimandroid/504854"
+		}]
+	}, {
+		"name": "Отмененные игры",
+		"content": [{
+			"banner": "images/not_found.jpg",
+			"name": "FNaF Non-AR",
+			"tags": ["Java, Firebase"],
+			"link": "https://gamejolt.com/games/fnafnonar/462486"
+		}, {
+			"banner": "images/not_found.jpg",
+			"name": "FNaF 4: 8 Bit Remake",
+			"tags": ["Pocket Code"],
+			"link": "https://share.catrob.at/app/project/93902"
+		}]
+	}, {
+		"name": "Потерянные игры",
+		"content": [{
+			"banner": "images/not_found.jpg",
+			"name": "Jollibee's Multiplayer",
+			"tags": ["Java, Firebase"],
+			"link": "https://gamejolt.com/p/the-project-got-deleted-it-s-all-gone-so-we-are-rebranding-it-int-3qzdr92w"
+		}]
+	}, {
+		"name": "Старые сайты",
+		"content": [{
+			"banner": "images/simplesite.jpg",
+			"name": "Мой третий сайт",
+			"tags": ["Конструктор"],
+			"link": "http://mrboom1.simplesite.com/442672397"
+		}, {
+			"banner": "images/site2.jpg",
+			"name": "Сайт для Ultimate Custom Night Meme Edition",
+			"tags": ["HTML, CSS, JS"],
+			"link": "https://mrboom1.gamejolt.io/ucnmeme"
+		}, {
+			"banner": "images/site1.jpg",
+			"name": " Мой самый первый сайт",
+			"tags": ["HTML, CSS"],
+			"link": "https://mrboom1.gamejolt.io"
+		}]
+	}]
+};
+
+
 
 function load(page) {
+	var html = "";
+	
 	switch(page) {
 		case "home":
 			//Load index.html
 			
-			let projects = document.getElementById("fillProjects");
+			let projects_div = document.getElementById("fillProjects");
+			var json = projects.pinned;
 			
-			var html = "";
-			for(var i = 0; i < names.length; i++) {
-				html += '<a href="./project?id=' + i + '"><figure class="project reveal"><img title="';
-				html += names[i] + '" src="' + banners[i] + '"/><figcaption><h3>' + names[i] + '</h3><p class="markup">';
+			for(var i = 0; i < json.length; i++) {
+				html += '<a class="reveal" href="./project?id=' + i + '"><div class="card big"><img title="';
+				html += json[i].name + '" src="' + json[i].banner + '"/><div class="info"><h3>' + json[i].name + '</h3><p class="markup">';
 				var tags_html = "";
-				for (var i1 = 0; i1 < tags[i].length - 1; i1++) {
-					tags_html += tags[i][i1];
-					if(i1 < tags[i].length - 2)
+				for (var i1 = 0; i1 < json[i].tags.length - 1; i1++) {
+					tags_html += json[i].tags[i1];
+					if(i1 < json[i].tags.length - 2)
 						tags_html += ', ';
 				}
-				html += tags_html + '</p><p>' + descriptions[i] + '</p></figcaption></figure></a>';
+				html += tags_html + '</p><p>' + json[i].description + '</p></div></div></a>';
 			}
 			
-			projects.innerHTML = html;
+			projects_div.innerHTML = html;
 			
 			break;
 		case "overview":
@@ -48,26 +118,51 @@ function load(page) {
 			let tags_view = document.getElementById("tags");
 			let description_view = document.getElementById("description");
 			let screenshots_view = document.getElementById("screenshotsa");
-			var id = window.location.href.split("?id=").pop()
+			var id = window.location.href.split("?id=").pop();
+			var json = projects.pinned;
 			var tags_html = "";
 			var screenshots_html = "";
 
-			if(isNaN(id) || id > names.length - 1 || id < 0)
+			if(isNaN(id) || id > json.length - 1 || id < 0)
 				window.location.href = "404";
 	
-			for (var i = 0; i < tags[id].length; i++) {
-				tags_html += '<h6 class="tag">' + tags[id][i] + '</h6>';
+			for (var i = 0; i < json[id].tags.length; i++) {
+				tags_html += '<h6>' + json[id].tags[i] + '</h6>';
 			}
-			for (let i1 = 0; i1 < screenshots[id].length; i1++) {
-				screenshots_html += '<a href="' + screenshots[id][i1] + '"><img src="' + screenshots[id][i1] + '"/></a>';
+			for (var i1 = 0; i1 < json[id].screenshots.length; i1++) {
+				screenshots_html += '<a href="' + json[id].screenshots[i1] + '"><div class="card image"><img src="' + json[id].screenshots[i1] + '"/></div></a>';
 			}
 
-			banner_view.src = banners[id];
-			name_view.innerHTML = names[id];
-			download.innerHTML = '<a href="' + downloads[id] + '"><button>Скачать</button></a>';
-			description_view.innerHTML = descriptions[id];
+			banner_view.src = json[id].banner;
+			name_view.innerHTML = json[id].name;
+			download.innerHTML = '<a href="' + json[id].link + '"><button>Скачать</button></a>';
+			description_view.innerHTML = json[id].description;
 			tags_view.innerHTML = tags_html;
 			screenshots_view.innerHTML = screenshots_html;
+			break;
+		case "archive":
+			//Load archive.html
+			
+			let archive_div = document.getElementById("fillProjects");
+			var json = projects.archive;
+			
+			for(var i = 0; i < json.length; i++) {
+				html += '<div class="block noimage reveal"><div><h2>' + json[i].name + '</h2><div class="grid">'
+				for(var i1 = 0; i1 < json[i].content.length; i1++) {
+					html += '<a target="_blank" class="reveal" href="' + json[i].content[i1].link + '"><div class="card big"><img title="';
+					html += json[i].content[i1].name + '" src="' + json[i].content[i1].banner + '"/><div class="info"><h3>' + json[i].content[i1].name + '</h3><p class="markup">';
+					var tags_html = "";
+					for (var i2 = 0; i2 < json[i].content[i1].tags.length; i2++) {
+						tags_html += json[i].content[i1].tags[i2];
+						if(i1 < json[i].content[i1].tags.length - 1)
+							tags_html += ', ';
+					}
+					html += tags_html + '</p><p>' + json[i].content[i1].link + '</p></div></div></a>';
+				}
+				html += '</div></div></div>';
+			}
+			
+			archive_div.innerHTML = html;
 			break;
 	}
 }
